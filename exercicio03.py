574dcdde08cf7f8aee8cf9c5bf9b04ce49200af8
gabi fabri <00001106292613sp@al.educacao.sp.gov.br>
2024-08-12 20:35:07 -0300

    Create exercicio03.py
    add exercicio 03

---
+++ exercicio03.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
+
+soma = numero1 + numero2 + numero3
+
+print("A soma dos números é:", soma)
