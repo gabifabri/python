7dd41ee4faed2fc17f3e453d129db45c25287a41
gabi fabri <00001106292613sp@al.educacao.sp.gov.br>
2024-08-19 20:29:32 -0300

    Create exercicio12.py
    add exercicio12

---
+++ exercicio12.py
@@ -0,0 +1,10 @@
+num = int(input("Informe um numero"))
+
+if num < 0:
+        print ("o numero é negativo")
+
+elif num == 0:
+    print ("o numero é zero")
+
+else:
+    print ("o numero é positivo")
