6a13f2e99d3b44125d2e57c5f85b0b4554f4d2c8
gabi fabri <00001106292613sp@al.educacao.sp.gov.br>
2024-09-02 20:23:07 -0300

    Create exercicio25.py

---
+++ exercicio25.py
@@ -0,0 +1,14 @@
+numero = float(input("Digite um número positivo e maior que zero: "))
+
+
+if numero > 0:
+    quadrado = numero ** 2
+    cubo = numero ** 3
+    print(f"Número ao quadrado: {quadrado}")
+    print(f"Número ao cubo: {cubo}")
+else:
+    print("Número inválido. O número deve ser positivo e maior que zero.")
+
+
+
+
