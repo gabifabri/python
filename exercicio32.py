5f9af8e90cef2dc56edbf220299429f5efc3fab8
gabi fabri <00001106292613sp@al.educacao.sp.gov.br>
2024-09-16 20:18:25 -0300

    Create exercicio32.py

---
+++ exercicio32.py
@@ -0,0 +1,5 @@
+temp_c = float(input('Digite a temperatura em Celsius: '))
+temp_f = (temp_c * 9/5) + 32
+temp_k = temp_c + 273.15
+print(f'A temperatura em Fahrenheit é {temp_f:.2f}°F')
+print(f'A temperatura em Kelvin é {temp_k:.2f}K')
